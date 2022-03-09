33715eef9884aa161094ac3718176051932750f0
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2022-03-10 00:03:30 +0200

    L7T3

--- app/src/main/java/com/example/t1/MainActivity.java
+++ app/src/main/java/com/example/t1/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
 
     TextView text;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,11 +19,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
+        inputText = (EditText) findViewById(R.id.editText);
     }
 
     public void function(View v)
     {
         System.out.println("Hello World!");
         text.setText("Hello World!");
+    }
+
+    public void textFunction(View y)
+    {
+        text.setText(inputText.getText());
     }
 }
