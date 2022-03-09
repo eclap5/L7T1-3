fba9e7ba0d8f14321565c219cc4603634ac1ed93
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2022-03-09 14:00:10 +0200

    L7T1 / Aleksi Haapalainen

---
+++ app/src/main/java/com/example/t1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void function(View v)
+    {
+        System.out.println("Hello World!");
+    }
+}
